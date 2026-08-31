ccfa2cb6b07db8f82ce619ec3a60e435cd4b1ad0
deeddevotional <kkhk452@gmail.com>
2026-09-01 03:07:43 +0530

    style: borderless navbar with exact brand slash separator and clean typography

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,19 +17,20 @@ export default function Navbar() {
   return (
     <header className="navbar">
       <div className="navbar-inner">
-        {/* Brand Logo & Clean Nav Links */}
         <div className="nav-left">
           <Link href="/" className="nav-brand">
             <Image
               src="/logo-white-small.png"
               alt="MJML Bits"
-              width={26}
-              height={26}
+              width={20}
+              height={20}
               className="brand-icon"
               priority
             />
             <span>MJML Bits</span>
           </Link>
+
+          <span className="nav-slash" aria-hidden="true">/</span>
 
           <nav className="nav-links">
             {navLinks.map((link) => {
